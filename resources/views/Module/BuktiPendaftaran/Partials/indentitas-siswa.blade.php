f3b8fd47ce9112c2e4ffba498210ced80130ed15
pyo_awndi <154031276+piyo12052004@users.noreply.github.com>
2025-06-04 22:30:45 +0700

    fix-formulir perbaikan pria perempuan

--- resources/views/Module/BuktiPendaftaran/Partials/indentitas-siswa.blade.php
+++ resources/views/Module/BuktiPendaftaran/Partials/indentitas-siswa.blade.php
@@ -1,4 +1,3 @@
-
 <div class="">
     <table border="1" style="width: 100%">
         <tr style="text-align: center">
@@ -10,12 +9,25 @@
             <td>Jalur Masuk</td>
         </tr>
         <tr style="text-align: center">
-            <td>{{$calonSiswa['nama_lengkap']}}</td>
-            <td>{{$calonSiswa['alamat'] ?? '-'}}</td>
-            <td>{{$calonSiswa['tanggal_lahir']}}</td>
-            <td>{{$calonSiswa['no_hp']}}</td>
-            <td>{{$calonSiswa['jenis_kelamin_id'] ?? '-'}}</td>
-            <td>{{$calonSiswa['jalur_masuk']}}</td>
+            <td>{{ $calonSiswa['nama_lengkap'] }}</td>
+            <td>{{ $calonSiswa['alamat'] ?? '-' }}</td>
+            <td>{{ $calonSiswa['tanggal_lahir'] }}</td>
+            <td>{{ $calonSiswa['no_hp'] }}</td>
+            <td>
+                @if (isset($calonSiswa['jenis_kelamin_id']))
+                    @if ($calonSiswa['jenis_kelamin_id'] == 1)
+                        Pria
+                    @elseif($calonSiswa['jenis_kelamin_id'] == 2)
+                        Perempuan
+                    @else
+                        -
+                    @endif
+                @else
+                    -
+                @endif
+            </td>
+
+            <td>{{ $calonSiswa['jalur_masuk'] }}</td>
         </tr>
     </table>
 </div>
